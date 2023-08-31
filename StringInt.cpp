dc715e62b9e9ac41bb7c3ff6ef559ce9bd276a1e
James Mok <hoodieman8@gmail.com>
2023-08-31 13:52:05 -0400

    refactor: StringInt constructor inserts last to first for less position calculations

--- StringInt.cpp
+++ StringInt.cpp
@@ -1,9 +1,12 @@
 #include "StringInt.hpp"
 
+// the number is represented in reverse order to reduce position calculations for arithmetic
+// ex: 1469 is represented as 9641
 StringInt::
 StringInt(const string num) {
-    for (int i = 0; i < num.size(); i++){
-        number.push_back(num[i]);
+    for (int i = num.size() - 1; i > -1; i--){
+        if (num[i] < 48 || num[i] > 57) throw "NaN"; // if it is not a number character
+        number.push_back(num[i]); 
     }
 }
 
