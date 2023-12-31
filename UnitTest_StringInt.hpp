8ca0687c62e67bea0fcfbc8a2095e2e310da0165
James Mok <hoodieman8@gmail.com>
2023-12-31 00:09:49 -0500

    feat: added UnitTest_StringInt.hpp for header guards and proper includes

---
+++ UnitTest_StringInt.hpp
@@ -0,0 +1,18 @@
+#ifndef UNIT_TEST_STRINGINT_HPP
+#define UNIT_TEST_STRINGINT_HPP
+
+#include <iostream>
+#include <iomanip>
+
+#include "StringInt.hpp"
+
+using namespace std;
+
+int UnitTest_RunAll();
+int UnitTest_DefaultConstruction();
+int UnitTest_CreateClass();
+int UnitTest_asInt();
+int UnitTest_Equivalnce();
+
+
+#endif
